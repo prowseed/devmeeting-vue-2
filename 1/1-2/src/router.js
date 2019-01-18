bbe13bea05dd9549533c1146175a8ce6708e1135
Krystian Fras <krystian@fras.me>
2019-01-18 19:01:37 +0000

    1-2

---
+++ 1/1-2/src/router.js
@@ -0,0 +1,29 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+
+import Home from '@/views/Home.vue';
+import About from '@/views/About.vue';
+import User from '@/views/User.vue';
+
+Vue.use(Router);
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+    },
+    {
+      // 2 4. And expose it through path params
+      path: '/user/:userId',
+      name: 'user',
+      component: User,
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: About,
+    },
+  ],
+});
---
+++ 1/1-2/src/views/User.vue
@@ -0,0 +1,4 @@
+<template>
+  <!--2 5. The $route is a way to get informations from router  -->
+  <div>{{ $route.params }}</div>
+</template>
