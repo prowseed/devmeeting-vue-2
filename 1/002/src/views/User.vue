82bd6892f9a93831ae411eb954dd8ca921cb661b
User for ghslides web application <xpla-ghslides@xpla.org>
2019-01-19 07:57:44 -0500

    Auto-generated slides.

--- 1/002/src/views/User.vue
+++ 1/002/src/views/User.vue
@@ -2,3 +2,9 @@
   <!--2 5. The $route is a way to get informations from router  -->
   <div>{{ $route.params }}</div>
 </template>
+
+<script>
+export default {
+  name: 'user',
+};
+</script>
---
+++ 1/004/src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <input type="text" v-model="userId" placeholder="user id">
+    <button @click="goToUser(userId)">Go to user</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'home',
+  data: () => ({
+    userId: '',
+  }),
+  methods: {
+    goToUser(id) {
+      this.$router.push({ name: 'user', params: { userId: id } });
+    },
+  },
+};
+</script>
