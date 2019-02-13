ad4fd20afbaeebbbaa3427fe0d8a33ec90ada920
User for ghslides web application <xpla-ghslides@xpla.org>
2019-02-13 07:03:44 -0500

    Auto-generated slides.

--- 3/002/src/repositories/http.service.js
+++ 3/002/src/repositories/http.service.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 //1/ 2. Let's setup our API URL
 const baseURL = 'https://test2-206614.firebaseio.com';
 
-//3 3. and export our HttpService
+//3/ 3. and export our HttpService
 export default axios.create({
   baseURL,
 });
