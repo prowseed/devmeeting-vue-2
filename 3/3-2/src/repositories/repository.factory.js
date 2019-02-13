a48267115e027d393e8c17c40526a6a011500ec1
Krystian Fras <krystian@fras.me>
2019-02-13 13:46:06 +0100

    tree

--- 3/3-2/src/repositories/repository.factory.js
+++ 3/3-2/src/repositories/repository.factory.js
@@ -1,9 +1,8 @@
 import SettingsRepository from './settings.repository';
 
-//4/ 7. We will create RepositoryFactory which takes care of all available repositories
+//4/ 7. We will create list of all available repositories
 const repositories = {
   settings: SettingsRepository,
-  // other repositories ...
 };
 
 //3/ 8. RepositoryFactory is actually simple object which allows to get our repositories
