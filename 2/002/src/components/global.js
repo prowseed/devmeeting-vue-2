44a93f1bc2cbd7d817d32bb0a925047795b88eb1
User for ghslides web application <xpla-ghslides@xpla.org>
2019-02-19 15:01:31 -0500

    Auto-generated slides.

---
+++ 2/002/src/components/global.js
@@ -0,0 +1,5 @@
+import Vue from 'vue';
+import BaseTextarea from './BaseTextarea.vue';
+
+//1/ 10. It can be a good practise to bind base components globally
+Vue.component('BaseTextarea', BaseTextarea);
